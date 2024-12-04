74f085dc77b9efdd6121a3610e0ec4aab5d56e19
Mitch Irvin <mitchellirvin@gmail.com>
2024-12-04 10:43:15 -0500

    small cleanup for day 4 part 1

--- day4.ts
+++ day4.ts
@@ -21,38 +21,39 @@ function countNumOfXmases(raw: string) {
             matrix[row][col] != wordToFind[charIndex]) {
             return
         }
-        if (charIndex == wordToFind.length - 1 && matrix[row][col] == wordToFind[charIndex]) {
+        if (charIndex == wordToFind.length - 1) {
             numFound++
             return
         }
 
         // you can only find words along a single direction, no zig-zagging allowed
         // don't need to track visited because we're only exploring in one direction at a time
+        charIndex++
         if (direction === undefined) {
-            findXmases(row + 1, col, charIndex + 1, "N")
-            findXmases(row + 1, col + 1, charIndex + 1, "NE")
-            findXmases(row, col + 1, charIndex + 1, "E")
-            findXmases(row - 1, col + 1, charIndex + 1, "SE")
-            findXmases(row - 1, col, charIndex + 1, "S")
-            findXmases(row - 1, col - 1, charIndex + 1, "SW")
-            findXmases(row, col - 1, charIndex + 1, "W")
-            findXmases(row + 1, col - 1, charIndex + 1, "NW")
+            findXmases(row + 1, col, charIndex, "N")
+            findXmases(row + 1, col + 1, charIndex, "NE")
+            findXmases(row, col + 1, charIndex, "E")
+            findXmases(row - 1, col + 1, charIndex, "SE")
+            findXmases(row - 1, col, charIndex, "S")
+            findXmases(row - 1, col - 1, charIndex, "SW")
+            findXmases(row, col - 1, charIndex, "W")
+            findXmases(row + 1, col - 1, charIndex, "NW")
         } else if (direction === "N") {
-            findXmases(row + 1, col, charIndex + 1, "N")
+            findXmases(row + 1, col, charIndex, "N")
         } else if (direction === "NE") {
-            findXmases(row + 1, col + 1, charIndex + 1, "NE")
+            findXmases(row + 1, col + 1, charIndex, "NE")
         } else if (direction === "E") {
-            findXmases(row, col + 1, charIndex + 1, "E")
+            findXmases(row, col + 1, charIndex, "E")
         } else if (direction === "SE") {
-            findXmases(row - 1, col + 1, charIndex + 1, "SE")
+            findXmases(row - 1, col + 1, charIndex, "SE")
         } else if (direction === "S") {
-            findXmases(row - 1, col, charIndex + 1, "S")
+            findXmases(row - 1, col, charIndex, "S")
         } else if (direction === "SW") { 
-            findXmases(row - 1, col - 1, charIndex + 1, "SW")
+            findXmases(row - 1, col - 1, charIndex, "SW")
         } else if (direction === "W") {
-            findXmases(row, col - 1, charIndex + 1, "W")
+            findXmases(row, col - 1, charIndex, "W")
         } else if (direction === "NW") {
-            findXmases(row + 1, col - 1, charIndex + 1, "NW")
+            findXmases(row + 1, col - 1, charIndex, "NW")
         }
     }
 
